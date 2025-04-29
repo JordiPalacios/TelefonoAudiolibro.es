079b2296d39f37176d06bd7fd5b24d1586083e09
JordiPalacios <jordipalacioseso@gmail.com>
2025-04-29 18:45:41 +0200

    rutas creadas en la que telefono es la main

--- src/web.jsx
+++ src/web.jsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from "react-router";
-import { TelefonoAudiolibro } from "./page";
+import { AvisoLegal, PoliticaPrivacidad, TelefonoAudiolibro, TerminosCondiciones } from "./page";
+import { Layout } from "./page/layout";
 
 function Web() {
     return (
         <Routes>
-            <Route path="/" element={<TelefonoAudiolibro />} />
-                {/* La usaré más adelante para cuando tenga dos páginas ya hechas o anidadas */}
-                {/* <Route index element={<TelefonoAudiolibro />} /> */}
+            <Route path="/" element={<Layout />}>
+                <Route index element={<TelefonoAudiolibro />} />
+                <Route path="aviso-legal" element={<AvisoLegal />} />
+                <Route path="terminos-condiciones" element={<TerminosCondiciones />} />
+                <Route path="politica-privacidad" element={<PoliticaPrivacidad />} />
+            </Route>
             <Route path='*' element={<TelefonoAudiolibro />} />
         </Routes>
     );
