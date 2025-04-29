c7773b7187b81dc74e35b8042d9179fb9cf08763
JordiPalacios <jordipalacioseso@gmail.com>
2025-04-29 07:54:29 +0200

    layoout creado

---
+++ src/page/layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Layout = () => {
+    // ? Pensar bien en hacer el layout porque sera compartido para todas las páginas que se creen
+    return (
+        <>
+            <section>Navbar</section>
+            
+            <section>Footer</section>
+        </>
+    )
+}
