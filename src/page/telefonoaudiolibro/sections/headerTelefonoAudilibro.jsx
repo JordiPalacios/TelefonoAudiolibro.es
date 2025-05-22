d2b8a6c9d9eb8a763b812b6fa8bf70f24a7da61b
JordiPalacios <jordipalacioseso@gmail.com>
2025-05-22 19:35:37 +0200

    componente header iniciado

---
+++ src/page/telefonoaudiolibro/sections/headerTelefonoAudilibro.jsx
@@ -0,0 +1,17 @@
+
+export const HeaderTelefonoAudilibro = () => {
+    return (
+        <>       
+            <div className="headerDescription">
+                <h1 className="font-serif font-bold text-5xl my-5">ESCUCHA Y GUARDA PARA SIEMPRE LA VOZ DE TUS INVITADOS</h1>
+                <p>Cada dia no se tiene a toda la familia y amigos juntos.</p>
+                <p>Aprovecha para que todos te dejen sus mensajes</p>
+                <button className="mt-20 bg-lime-300 rounded-2xl border-1 border-black py-2 px-3.5 font-bold hover:scale-105 transition-transform duration-200"> 
+                    ENVIAR SOLICITUD 
+                </button>
+                <p className="font-bold">El 99% de las personas que no lo tienen en su boda se arrepienten...</p>
+            </div>     
+            <img src="./public/media/header/telf04.webp" alt="telefonoaudiolibro.es" />
+        </>
+    )
+}
