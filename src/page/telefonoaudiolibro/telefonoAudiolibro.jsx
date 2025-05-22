061fe9d5e1005bd4860bc4f38230dbd1a75f3b7a
JordiPalacios <jordipalacioseso@gmail.com>
2025-05-22 19:34:51 +0200

    empezado el encabezado header

--- src/page/telefonoaudiolibro/telefonoAudiolibro.jsx
+++ src/page/telefonoaudiolibro/telefonoAudiolibro.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
 import { Section } from '../../components/shared/section'
+import { HeaderTelefonoAudilibro } from './sections/headerTelefonoAudilibro'
 
 export const TelefonoAudiolibro = () => {
     return (
-        <main>
-            <h1 className="text-5xl font-bold">Teléfonoaudiolibro.es</h1>
-            <Section as="header" className="bg-green-300">
-                <p>Hola</p>
-                <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded cursor-pointer"> Adios </button>
-                <h1 className="text-5xl font-bold">Hello</h1>
+        <>
+            <Section as="header" className="headerContainer">
+                <HeaderTelefonoAudilibro />
             </Section>
-            <Section as="header" className="bg-red-300">
-                <p>Hola</p>
-                <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded cursor-pointer"> Adios </button>
-                <h1 className="text-5xl font-bold">Hello</h1>
-            </Section>
-        </main>
+            <main>                
+                <Section as="section" className="bg-red-300">
+                    <p>Hola</p>
+                    <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded cursor-pointer"> Adios </button>
+                    <h1 className="text-5xl font-bold">Hello</h1>
+                </Section>
+            </main>
+        </>
     )
 }
