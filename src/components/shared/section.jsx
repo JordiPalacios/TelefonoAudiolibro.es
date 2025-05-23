84f4df87a8a838109ced19cc6ed421a827869883
JordiPalacios <jordipalacioseso@gmail.com>
2025-05-23 16:04:07 +0200

    modificado para que el usuario no seleccione textos

--- src/components/shared/section.jsx
+++ src/components/shared/section.jsx
@@ -2,7 +2,7 @@
 export const Section = ({ children, className = '', as = 'section' }) => {
     const Tag = as
     return (
-        <Tag className= {`w-full h-auto px-6 py-2 my-2 ${className}`}>
+        <Tag className= {`w-full h-auto px-6 py-2 my-2 select-none ${className}`}>
             {children}
         </Tag>
     )
