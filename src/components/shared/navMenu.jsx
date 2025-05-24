a0faded7a1a8527ff13d1df362c22cb448ab2c8b
JordiPalacios <jordipalacioseso@gmail.com>
2025-05-24 16:10:40 +0200

    comentarios elimindados

--- src/components/shared/navMenu.jsx
+++ src/components/shared/navMenu.jsx
@@ -31,7 +31,7 @@ export const NavMenu = () => {
                 >
             <Hamburger onChange={toggleMenu} />
             <h1 className="hidden lg:block">Solo pc</h1>
-            <h1 className="text-xs text-white/90 font-serif font-bold truncate hover:text-white/70 transition-colors duration-300">
+            <h1 className="text-md text-white/90 font-serif font-bold truncate hover:text-white/70 transition-colors duration-300">
                 <a href="/">                    
                     telefonoaudiolibro.es
                 </a>
@@ -45,10 +45,9 @@ export const NavMenu = () => {
         </nav>
         {isOpen && (
             <aside className="fixed top-18 left-0 w-fit h-full z-50  flex flex-col items-start py-2 px-6 bg-white/20 backdrop-blur-md shadow">
-                {/* Aquí va el contenido del menú móvil */}
                 <a href="/" className="mb-4 text-lg font-bold text-white">Inicio</a>
                 <a href="/contacto" className="mb-4 text-lg font-bold text-white">Contacto</a>
-                {/* Puedes añadir más enlaces o contenido aquí */}
+                {/* Añadir más enlaces o contenido aquí */}
             </aside>
         )
         }
