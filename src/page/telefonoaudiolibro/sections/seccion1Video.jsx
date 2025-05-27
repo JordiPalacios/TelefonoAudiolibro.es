022fbfd3d4e4817cb3fccce997058e7b5c67ed83
JordiPalacios <jordipalacioseso@gmail.com>
2025-05-27 19:14:36 +0200

    seccion 1 para movil mas o menos arreglada

--- src/page/telefonoaudiolibro/sections/seccion1Video.jsx
+++ src/page/telefonoaudiolibro/sections/seccion1Video.jsx
@@ -2,11 +2,11 @@
 export const Seccion1Video = () => {
     return (
         <>
-            <div>
-                <h2>
+            <div className="flex flex-col items-center justify-center text-center pt-5">
+                <h2 className="text-3xl font-serif font-bold mb-3">
                     ESTOS PRODRÍAIS SER VOSOTROS
                 </h2>
-                <p>
+                <p className="font-bold mb-5">
                     Reviviendo de nuevo la boda de el día siguiente.
                 </p>
                 <button className='mt-0 bg-lime-300 rounded-2xl border-1 border-black py-2 px-3.5 font-bold md:text-xl hover:scale-105 transition-transform duration-200'>
@@ -14,6 +14,7 @@ export const Seccion1Video = () => {
                 </button>
             </div>
             <video 
+                className="mb-5 rounded-lg"
                 src="/video/videoMobile.mov" 
                 itemType='video/mov' 
                 preload='metadata' 
