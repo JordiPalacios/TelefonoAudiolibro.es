497525429494eaa06ee42ea70df8e2e43c8967a1
JordiPalacios <jordipalacioseso@gmail.com>
2025-05-29 20:00:36 +0200

    tarjeta creada a falta de los iconos que me tienen que enviar

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,20 +1,17 @@
 
-export const Card = ({ id, url, tittle, desc1, desc2 }) => {
+export const Card = ({ url, tittle, desc1, desc2 }) => {
     return (
-        <>
-            <p className="font-bold">
-                {id}
-            </p>
-            <img src={url} alt={tittle} width="150px" height="150px"/>
-            <h2 className="font-bold text-2xl">
+        <div className="flex flex-col items-center justify-center text-center gap-4 p-4 my-4 bg-white/30 rounded-lg shadow-md">
+            <h2 className="font-bold text-xl text-balance">
                 {tittle}
             </h2>
+            <img src={url} alt={tittle} width="150px" height="150px" className="rounded-full" />
             <p>
                 {desc1}
             </p>
             <p>
                 {desc2}
             </p>
-        </>
+        </div>
     )
 }
