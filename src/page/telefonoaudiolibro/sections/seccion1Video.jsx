ca71607932a20b0d24c46686b9e6ec5a25e6269c
JordiPalacios <jordipalacioseso@gmail.com>
2025-05-30 23:26:18 +0200

    imagenes añadidas y videos. Boton añadido para version tablet y mobile

--- src/page/telefonoaudiolibro/sections/seccion1Video.jsx
+++ src/page/telefonoaudiolibro/sections/seccion1Video.jsx
@@ -3,13 +3,13 @@ export const Seccion1Video = () => {
     return (
         <>
             <div className="lg:flex lg:flex-col lg:items-center lg:justify-center lg:text-start pt-5">
-                <h2 className="text-3xl font-serif font-bold mb-3">
+                <h2 className="text-3xl font-serif font-bold mb-10">
                     ESTOS PRODRÍAIS SER VOSOTROS
                 </h2>
-                <p className="font-bold mb-5">
+                <p className="font-bold mb-5 text-lg lg:text-xl">
                     Reviviendo de nuevo la boda de el día siguiente.
                 </p>
-                <button className='mt-0 bg-lime-300 rounded-2xl border-1 border-black py-2 px-3.5 font-bold md:text-xl hover:scale-105 transition-transform duration-200'>
+                <button className='hidden lg:block mt-0 bg-lime-300 rounded-2xl border-1 border-black py-2 px-3.5 font-bold md:text-xl hover:scale-105 transition-transform duration-200'>
                     LO QUIERO EN MI BODA
                 </button>
             </div>
@@ -23,6 +23,9 @@ export const Seccion1Video = () => {
                 poster='/media/header/telf07.webp' 
                 playsInline 
             />
+            <button className='block lg:hidden mt-0 bg-lime-300 rounded-2xl border-1 border-black py-2 px-3.5 font-bold md:text-xl hover:scale-105 transition-transform duration-200'>
+                    LO QUIERO EN MI BODA
+                </button>
         </>
     )
 }
