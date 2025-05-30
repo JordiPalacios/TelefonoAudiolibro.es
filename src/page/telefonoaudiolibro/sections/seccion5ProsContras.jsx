821dbae3b94cf0017ba8c7f6d76eaca46327ec99
JordiPalacios <jordipalacioseso@gmail.com>
2025-05-31 00:56:57 +0200

    creadas las tablas para los pros y contras, con el mock y su mapeo. Falta optimizar los estilos para mobile-tablet y pc.

--- src/page/telefonoaudiolibro/sections/seccion5ProsContras.jsx
+++ src/page/telefonoaudiolibro/sections/seccion5ProsContras.jsx
@@ -1,11 +1,53 @@
+import ProsVSContsData from "../../../assets/mocks/prosVSconts.json"
+import { CrossIcon, TickIcon } from "../../../components"
 
 export const Seccion5ProsContras = () => {
+    const prosVSconts = ProsVSContsData
     return (
         <>
-            <h2>
+            <h2 className="text-3xl font-bold font-serif mb-10 text-center">
                 ¿CON CÚAL TE QUEDAS?
             </h2>
-            <img src="/media/header/telf07.webp" alt="telefonoaudiolibro" width="150px" height="150px"/>
+            <div className="flex flex-col items-center justify-center w-full">                
+                <table className="max-w-md w-full text-center mb-10 px-5 border-separate border-spacing-y-4 bg-white/50 p-3 shadow-md rounded-xl">
+                    <thead>                    
+                        <tr>
+                            <th className="align-middle">
+                                <img src="/img/manual.webp" alt="telefonoaudiolibro.es" className="max-w-30 h-auto aspect-[3/4] rounded-xl" />
+                            </th>
+                            <th />
+                            <th className="align-middle">
+                                <img src="/img/telf12.webp" alt="telefonoaudiolibro.es" className="max-w-30 h-auto aspect-[3/4] rounded-xl" />
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>                    
+                        {prosVSconts.map((item) => (
+                            <tr key={item.id} className="align-middle">
+                                <td className="align-middle">
+                                    <div className="flex justify-center">    
+                                        {item.isCrossLeft 
+                                            ? <CrossIcon className="w-8 h-8 text-red-600" />
+                                            : <TickIcon className="w-8 h-8 text-green-600" />
+                                        }
+                                    </div>
+                                </td>
+                                <td className="align-middle font-semibold text-lg">
+                                    {item.tittle}
+                                </td>
+                                <td className="align-middle">
+                                    <div className="flex justify-center">
+                                        {item.isCrossLeft 
+                                            ? <CrossIcon className="w-8 h-8 text-red-600" />
+                                            : <TickIcon className="w-8 h-8 text-green-600" />
+                                        }
+                                    </div>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </>
     )
 }
