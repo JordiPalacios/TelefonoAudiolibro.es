b19fc0014bb6d35367c0cf0bb08f3d6cab30d430
JordiPalacios <jordipalacioseso@gmail.com>
2025-05-30 17:28:46 +0200

    añadidos los estilos de css a tailwind

--- src/components/shared/navMenu.jsx
+++ src/components/shared/navMenu.jsx
@@ -18,13 +18,15 @@ export const NavMenu = () => {
         setIsOpen(!isOpen);
     }
 
+    console.log("isScrolled:", scrolled);
+
     return (
         <>
         <nav 
             className={`navContainer fixed top-0 left-0 w-full z-50 flex flex-row-reverse items-center justify-between lg:justify-center lg:gap-0 p-4 transition-all duration-300
                 ${scrolled
-                    ? "bg-white/60 backdrop-blur-md shadow"
-                    : "bg-transparent"
+                    ? "bg-black/60 backdrop-blur-md shadow"
+                    : "bg-transparent backdrop-blur-none shadow-none"
                 }`}
                 >
             <Hamburger onChange={toggleMenu} />
