2c2cf3ab1bbcef9a4190411f6341170383750bdf
JordiPalacios <jordipalacioseso@gmail.com>
2025-05-31 16:47:21 +0200

    Estilos para las reviews añadido. Componente de reviews con slider creado.

--- src/page/telefonoaudiolibro/sections/reviews.jsx
+++ src/page/telefonoaudiolibro/sections/reviews.jsx
@@ -1,6 +1,25 @@
+import { MainSlider } from "../../../components/shared/mainSlider"
 
 export const Reviews = () => {
     return (
-        <div>reviews</div>
+        <>
+            <h3 className="text-2xl md:text-3xl lg:text-4xl font-bold font-serif text-center mb-8">
+                OPINIONES
+            </h3>
+            <MainSlider 
+                type="reviews"
+                draggable={true}
+                swipe={true}
+                touchMove={true}
+                dots={true}
+                arrows={true}
+                infinite={true}
+                speed={500}
+                slidesToShow={1}
+                slidesToScroll={1}
+                autoplay={false}
+                autoplaySpeed={0}
+            />
+        </>
     )
 }
