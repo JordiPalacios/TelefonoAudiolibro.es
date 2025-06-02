b094a87f1d753466b0037a0ca6ba013dc6f99403
JordiPalacios <jordipalacioseso@gmail.com>
2025-06-02 22:08:01 +0200

    modificada la tarjeta para que se vea mas potente

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,11 +1,16 @@
 
-export const Card = ({ url, tittle, desc1, desc2 }) => {
+export const Card = ({ id, tittle, desc1, desc2 }) => {
     return (
-        <div className="flex flex-col items-center justify-evenly text-center w-xs md:w-md aspect-[3/4] gap-4 p-4 my-2 lg:mx-4 bg-gray-100/50 rounded-lg shadow-md">
-            <h2 className="font-semibold text-xl md:text-2xl text-balance">
+        <div className="relative flex flex-col items-center justify-evenly text-center w-xs md:w-md aspect-[4/3] gap-4 p-4 my-10 md:my-20 lg:mx-4 bg-gray-100/50 rounded-4xl shadow-md">
+            <span
+                className="title-accent absolute -top-135 left-6 text-7xl font-extrabold select-none pointer-events-none rotate-12"
+                style={{ lineHeight: 15, color: "transparent", WebkitTextStroke: "2px #000", textStroke: "2px #9ca3af" }}
+            >
+                {id}
+            </span>
+            <h2 className="font-extrabold text-xl md:text-2xl text-balance">
                 {tittle}
             </h2>
-            <img src={url} alt={tittle} width="150" height="150" className="rounded-full" loading="lazy"/>
             <p className="font-bold md:text-lg text-pink-500">
                 {desc1}
             </p>
