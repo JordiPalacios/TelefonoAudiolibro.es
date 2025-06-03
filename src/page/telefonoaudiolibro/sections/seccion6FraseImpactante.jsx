42384e834cb84ab2f4b24d2aba7c6aef41a241d6
JordiPalacios <jordipalacioseso@gmail.com>
2025-06-03 19:45:12 +0200

    estilada la frase para que en pc se vea mas ancha

--- src/page/telefonoaudiolibro/sections/seccion6FraseImpactante.jsx
+++ src/page/telefonoaudiolibro/sections/seccion6FraseImpactante.jsx
@@ -1,15 +1,21 @@
 
 export const Seccion6FraseImpactante = () => {
     return (
-        <div className="relative w-full h-96 md:h-125 flex items-center justify-center shadow-2xl">
+        <div className="relative w-full h-96 lg:h-[50rem] md:h-125 flex items-center justify-center shadow-2xl">
+            <img 
+            src="/img/telf22.webp" 
+            alt="Fondo de la frase impactante" 
+            loading="lazy"
+            className="lg:hidden absolute inset-0 w-full h-full object-cover"
+            />
             <img 
             src="/img/telf23.webp" 
             alt="Fondo de la frase impactante" 
             loading="lazy"
-            className="absolute inset-0 w-full h-full object-cover"
+            className="hidden lg:block absolute inset-0 w-full h-full object-cover"
             />
             <div className="relative z-10">
-                <p className="max-w-md md:max-w-3xl accent text-2xl md:text-4xl text-center text-white font-semibold p-4">
+                <p className="max-w-md md:max-w-3xl accent text-4xl md:text-6xl text-center text-lime-300 font-bold p-4">
                     TU BODA DURA 6 HORAS, ESTOS AUDIOS SON PARA TODA LA VIDA
                 </p>
             </div>
