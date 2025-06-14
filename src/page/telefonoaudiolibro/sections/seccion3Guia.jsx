a3ad17cd64bde4a980a56ba6be684507f39a19da
JordiPalacios <jordipalacioseso@gmail.com>
2025-06-14 18:54:41 +0200

    añadido vídeo explicativo influencer y telefonos por el lado para modo pc asñi decora mas

--- src/page/telefonoaudiolibro/sections/seccion3Guia.jsx
+++ src/page/telefonoaudiolibro/sections/seccion3Guia.jsx
@@ -2,34 +2,64 @@ import { Card } from "../../../components"
 
 export const Seccion3Guia = () => {
   return (
-    <div className="flex flex-col items-center justify-center py-5">
-      <h2 className="title text-center mt-5 underline decoration-wavy decoration-2 underline-offset-2">
-        ¿CÓMO FUNCIONA?
+    <div className="relative flex flex-col items-center justify-center py-5">
+      <h2 className="title text-center mt-5">
+        ¿CÓMO 
+        <span className="title-accent font-light"> FUNCIONA? </span>
       </h2>
+      <img 
+      src="/img/auxiliares/telefonoMuestra01.webp" 
+      alt="telefono audiolibro guia" 
+      className="componentAnimation hidden lg:block max-w-xs object-cover absolute top-50 right-75"
+      />
+      <img 
+      src="/img/auxiliares/telefonoMuestra01.webp" 
+      alt="telefono audiolibro guia" 
+      className="componentAnimation hidden lg:block max-w-xs object-cover absolute top-75 left-75"
+      />
+      <img 
+      src="/img/auxiliares/telefonoMuestra01.webp" 
+      alt="telefono audiolibro guia" 
+      className="componentAnimation hidden lg:block max-w-xs object-cover absolute top-125 right-75"
+      />
+      <img 
+      src="/img/auxiliares/telefonoMuestra01.webp" 
+      alt="telefono audiolibro guia" 
+      className="componentAnimation hidden lg:block max-w-xs object-cover absolute top-150 left-75"
+      />
+      <video 
+      src="/video/videoGuia.mov" 
+      itemType="video/mov"
+      controls
+      controlsList="nodownload"
+      poster="/video/poster/poster08.webp"
+      playsInline
+      className="componentAnimation m-5 lg:m-10 rounded-2xl shadow-2xl max-w-3/4 md:max-w-sm object-cover"
+      />
       <div className="flex flex-col lg:flex-row lg:gap-8 items-center justify-center">
         <Card 
-          url="/img/telf01.webp"
+          url="/img/auxiliares/telefonoMuestra00.webp"
           id="1"
           tittle=" RECIBE EL TELÉFONO "
           desc1=" Te lo enviamos días antes a domicilio. "
           desc2="¡Ya podéis grabar vuestro mensaje de bienvenida! "   
           />
         <Card 
-          url="/img/telf18.webp"
+          url="/img/auxiliares/telefonoMuestra00.webp"
           id="2"
           tittle=" TUS PERSONAS FAVORITAS DEJAN MENSAJES DE AUDIO "
           desc1=" Desde emotivos hasta divertidos y cachondos.  "
           desc2=" Todo esto es lo que recordarás para el resto de tus días. "   
           />
         <Card 
-          url="/img/telf13.webp"
+          url="/img/auxiliares/telefonoMuestra00.webp"
           id="3"
           tittle=" RECIBE LOS MENSAJES EN 24 HORAS "
           desc1=" ¡Os recogemos el teléfono! " 
           desc2=" Transformamos los mensajes en recuerdos digitales que tendréis para siempre"
           />
       </div>
-      <button className="button m-5">
+      <button className="button mb-5">
         texto provisional
       </button>
     </div>
