008f355be306921cfce28259fb968348291fae33
JordiPalacios <jordipalacioseso@gmail.com>
2025-06-14 17:46:27 +0200

    componente para navegar entre secciones creado

---
+++ src/components/shared/navSections.jsx
@@ -0,0 +1,19 @@
+export const NavSections = ( {section} ) => {
+    const name = section.charAt(0).toUpperCase() + section.slice(1);
+    
+    return (
+        <a href={section}
+        onClick={e => {
+                    e.preventDefault();
+                    const el = document.getElementById(section);
+                    if (el) {
+                    el.scrollIntoView({ behavior: "smooth" });
+                    // Quita el hash de la URL
+                    window.history.replaceState(null, "", window.location.pathname);
+                    }
+                }}
+        className='hidden lg:block text-white hover:text-white/70 transition-colors duration-300'>
+            {name}
+        </a>
+    )
+}
