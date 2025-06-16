241b8fde9401b0fdc589f2835de1db0c7d55307f
JordiPalacios <jordipalacioseso@gmail.com>
2025-06-16 20:15:43 +0200

    quitadas las imagenes que ocupaban espacio visual. Se ha puesto que las cards esten en zig zag cuando esten en columna. Se han estilado tambien los elementos absolutos para todos los tipo de pantalla.

--- src/page/telefonoaudiolibro/sections/seccion3Guia.jsx
+++ src/page/telefonoaudiolibro/sections/seccion3Guia.jsx
@@ -2,7 +2,7 @@ import { Card } from "../../../components"
 
 export const Seccion3Guia = () => {
   return (
-    <div className="relative flex flex-col items-center justify-center py-5">
+    <div className="relative flex flex-col py-5 overflow-hidden">
       <h2 className="title text-center mt-5">
         ¿CÓMO 
         <span className="title-accent font-light"> FUNCIONA? </span>
@@ -10,23 +10,13 @@ export const Seccion3Guia = () => {
       <img 
       src="/img/auxiliares/telefonoMuestra01.webp" 
       alt="telefono audiolibro guia" 
-      className="componentAnimation hidden lg:block max-w-xs object-cover absolute top-50 right-75"
+      className="componentAnimation hidden lg:block max-w-xs object-cover p-5 absolute top-1/3 xl:top-1/2 -right-1/24 xl:-right-1/24 2xl:right-1/6"
       />
       <img 
       src="/img/auxiliares/telefonoMuestra01.webp" 
       alt="telefono audiolibro guia" 
-      className="componentAnimation hidden lg:block max-w-xs object-cover absolute top-75 left-75"
-      />
-      <img 
-      src="/img/auxiliares/telefonoMuestra01.webp" 
-      alt="telefono audiolibro guia" 
-      className="componentAnimation hidden lg:block max-w-xs object-cover absolute top-125 right-75"
-      />
-      <img 
-      src="/img/auxiliares/telefonoMuestra01.webp" 
-      alt="telefono audiolibro guia" 
-      className="componentAnimation hidden lg:block max-w-xs object-cover absolute top-150 left-75"
-      />
+      className="componentAnimation hidden lg:block max-w-xs object-cover p-5 absolute top-1/12 xl:top-1/6 -left-1/24 xl:-left-1/24 2xl:left-1/6"
+      />   
       <video 
       src="/video/videoGuia.mov" 
       itemType="video/mov"
@@ -34,15 +24,16 @@ export const Seccion3Guia = () => {
       controlsList="nodownload"
       poster="/video/poster/poster08.webp"
       playsInline
-      className="componentAnimation m-5 lg:m-10 rounded-2xl shadow-2xl max-w-3/4 md:max-w-sm object-cover"
+      className="componentAnimation mb-5 md:mb-20 self-center rounded-2xl shadow-2xl max-w-3/4 md:max-w-xs xl:max-w-sm object-cover"
       />
-      <div className="flex flex-col lg:flex-row lg:gap-8 items-center justify-center">
+      <div className="flex flex-col items-stretch justify-center xl:flex-row overflow-hidden px-5 xl:gap-5 2xl:gap-40">
         <Card 
           url="/img/auxiliares/telefonoMuestra00.webp"
           id="1"
           tittle=" RECIBE EL TELÉFONO "
           desc1=" Te lo enviamos días antes a domicilio. "
           desc2="¡Ya podéis grabar vuestro mensaje de bienvenida! "   
+          className = "self-center sm:self-start xl:self-auto"
           />
         <Card 
           url="/img/auxiliares/telefonoMuestra00.webp"
@@ -50,6 +41,7 @@ export const Seccion3Guia = () => {
           tittle=" TUS PERSONAS FAVORITAS DEJAN MENSAJES DE AUDIO "
           desc1=" Desde emotivos hasta divertidos y cachondos.  "
           desc2=" Todo esto es lo que recordarás para el resto de tus días. "   
+          className = "self-center sm:self-end xl:self-auto"
           />
         <Card 
           url="/img/auxiliares/telefonoMuestra00.webp"
@@ -57,9 +49,10 @@ export const Seccion3Guia = () => {
           tittle=" RECIBE LOS MENSAJES EN 24 HORAS "
           desc1=" ¡Os recogemos el teléfono! " 
           desc2=" Transformamos los mensajes en recuerdos digitales que tendréis para siempre"
+          className = "self-center sm:self-start xl:self-auto"
           />
       </div>
-      <button className="button mb-5">
+      <button className="button mb-5 self-center">
         texto provisional
       </button>
     </div>
