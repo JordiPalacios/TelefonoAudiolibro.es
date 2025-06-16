b55050de640e4f97a2dd3d68d6b8b7a9c3b445ce
JordiPalacios <jordipalacioseso@gmail.com>
2025-06-16 18:55:21 +0200

    tabla y fondo cambiados de color para tener uan estetica

--- src/page/telefonoaudiolibro/sections/seccion5ProsContras.jsx
+++ src/page/telefonoaudiolibro/sections/seccion5ProsContras.jsx
@@ -9,7 +9,7 @@ export const Seccion5ProsContras = () => {
                 ¿CON CÚAL TE QUEDAS?
             </h2>
             <div className="comparisonAnimation flex flex-col items-center justify-center w-full">                
-                <table className="max-w-md w-full text-center px-5 py-2 border-separate border-spacing-y-4 bg-gray-100/50 shadow-md rounded-xl">
+                <table className="max-w-md w-full text-center px-5 py-2 border-separate border-spacing-y-4 bg-pink-100/50 shadow-md rounded-xl">
                     <thead>                    
                         <tr>
                             <th className="align-middle">
@@ -23,23 +23,23 @@ export const Seccion5ProsContras = () => {
                     </thead>
                     <tbody>                    
                         {prosVSconts.map((item) => (
-                            <tr key={item.id} className="align-middl bg-gray-100/50 shadow-md shadow-black/20 rounded-xl">
+                            <tr key={item.id} className="align-middl bg-pink-100/50 shadow-md shadow-black/20 rounded-xl">
                                 <td className="align-middle">
                                     <div className="flex justify-center">    
                                         {item.isCrossLeft 
-                                            ? <CrossIcon className="w-8 h-8 text-red-600" />
-                                            : <TickIcon className="w-8 h-8 text-green-600" />
+                                            ? <CrossIcon className="w-8 h-8 text-pink-400" />
+                                            : <TickIcon className="w-8 h-8 text-pink-600" />
                                         }
                                     </div>
                                 </td>
                                 <td className="align-middle font-semibold text-md md:text-lg">
                                     {item.tittle}
                                 </td>
-                                <td className="align-middle bg-lime-200/50 shadow-lg shadow-black/20 rounded-xl">
+                                <td className="align-middle bg-pink-200/50 shadow-lg shadow-black/20 rounded-xl">
                                     <div className="flex justify-center">
                                         {item.isCrossRight 
                                             ? <CrossIcon className="w-8 h-8 text-red-600" />
-                                            : <TickIcon className="w-8 h-8 text-green-600" />
+                                            : <TickIcon className="w-8 h-8 text-pink-600" />
                                         }
                                     </div>
                                 </td>
