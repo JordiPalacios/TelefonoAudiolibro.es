c7c074d6df67d46720f640342bee7aafd6214303
JordiPalacios <jordipalacioseso@gmail.com>
2025-06-17 20:50:18 +0200

    estilada la landing de pago realizado

--- src/page/telefonoaudiolibro/pagoRealizado.jsx
+++ src/page/telefonoaudiolibro/pagoRealizado.jsx
@@ -1,13 +1,13 @@
 export const PagoRealizado = () => {
     return (
-        <>
-            <h1>
+        <div className="pagoRealizado flex flex-col items-center justify-center h-screen">
+            <h1 className="title text-center mb-5 text-pink-400">
                 Gracias por confiar en telefonoaudiolibro.es
             </h1>
-            <h2>
-                ¡Nos vemos pronto creando los mejores recuerdos en tu <span>día más especial!</span>
+            <h2 className="subtitle text-center mb-5 text-pink-300 font-bold">
+                ¡Nos vemos pronto creando los mejores recuerdos en tu <span className="text-lime-300">día más especial!</span>
             </h2>
-            <p>
+            <p className="text-center mb-5 text-pink-200 font-semibold">
                 Pronto recibirás un correo con la confirmación de tu reserva.
             </p>
             <button>
@@ -19,6 +19,6 @@ export const PagoRealizado = () => {
                     Volver a la página principal
                 </a>
             </button>
-        </>
+        </div>
     )
 }
