40d2c6ef406f2653f45779d2ca43eb14a8b1b824
JordiPalacios <jordipalacioseso@gmail.com>
2025-06-19 20:07:20 +0200

    sección de audios estilada con frases nuevas

--- src/page/telefonoaudiolibro/sections/seccion2Audios.jsx
+++ src/page/telefonoaudiolibro/sections/seccion2Audios.jsx
@@ -7,7 +7,7 @@ export const Seccion2Audios = () => {
             <img 
             src="/img/auxiliares/bolaDiscoteca.webp" 
             alt="telefono audiolibro guia" 
-            className="componentAnimation block max-w-40 md:max-w-60 lg:max-w-xs object-cover absolute top-0 -left-7.5 md:-left-10 lg:-left-1/24 xl:-left-10 2xl:left-0"
+            className="componentAnimation block 2xl:px-5 max-w-40 md:max-w-60 lg:max-w-xs object-cover absolute top-0 -left-7.5 md:-left-10 lg:-left-1/24 xl:-left-10 2xl:left-100"
             />
             <div className="hidden mt-10 py-10 lg:block w-1/2 h-full">
                 <MainSlider
@@ -25,18 +25,23 @@ export const Seccion2Audios = () => {
                     autoplaySpeed={0}
                     />
             </div>         
-            <div className="flex flex-col items-center justify-center mt-10 z-10">
-                <h2 className="title text-center max-w-75 md:max-w-xl lg:max-w-5xl">
-                    NO TODO EL MUNDO TIENE LA VOZ DE SU <span className="title-accent font-extrabold text-pink-500/90">ABUELA</span> GUARDADA PARA SIEMPRE
-                </h2>
-                <p className="mb-5 text-xl md:text-2xl text-pink-500 font-semibold underline-offset-4 underline decoration-dashed">
+            <div className="flex flex-col items-center justify-center text-cente mt-10 z-10">
+                <span className="tinos font-extrabold text-5xl md:text-7xl tracking-tighter"> guarda </span>
+                <p className="tinos text-4xl md:text-6xl rotate-2 r max-w-75 md:max-w-xl lg:max-w-5xl tracking-tighter">
+                    la voz
+                </p>
+                <p className="tinos font-extrabold text-5xl md:text-7xl tracking-tighter">
+                    de tu <span className="title-accent font-extrabold text-pink-500/90">ABUELA</span>
+                </p>
+                <small className="tinos italic font-bold text-lg md:text-xl lg:text-2xl text-pink-700 mb-6 tracking-tighter">para siempre.</small>
+                <p className="mb-5 italic text-xl md:text-2xl text-pink-500 font-semibold underline-offset-4 underline decoration-dashed">
                     DALE AL PLAY
                 </p>                        
                     <audio className="w-xs my-2" src="/audio/audio.mp4" controls controlsList="nodownload" />
                     <audio className="w-xs my-2" src="/audio/audio.mp4" controls controlsList="nodownload" />
                     <audio className="w-xs my-2" src="/audio/audio.mp4" controls controlsList="nodownload" />                
                 <button className="button m-10">
-                    Lo quiero en mi boda
+                    reservar ahora
                 </button>
             </div>
         </>
