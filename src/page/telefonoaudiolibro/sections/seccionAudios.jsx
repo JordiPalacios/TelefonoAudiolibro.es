00409d3fea29d9c0db33af84d0095cbfdf1c77dd
JordiPalacios <jordipalacioseso@gmail.com>
2025-07-07 17:18:07 +0200

    cambiada la palabra abuela por la voz, el acentuado

--- src/page/telefonoaudiolibro/sections/seccionAudios.jsx
+++ src/page/telefonoaudiolibro/sections/seccionAudios.jsx
@@ -8,20 +8,6 @@ export const SeccionAudios = () => {
             className="block 2xl:px-5 max-w-40 md:max-w-60 xl:max-w-70 object-cover absolute top-0 -left-7.5 md:-left-10 lg:-left-1/24 xl:-left-10 2xl:left-100"
             />
             <div className="hidden lg:block pl-20">
-                {/* <MainSlider
-                    type="imagenes"
-                    draggable={true}
-                    swipe={true}
-                    touchMove={true}
-                    dots={false}
-                    arrows={true}
-                    infinite={true}
-                    speed={500}
-                    slidesToShow={1}
-                    slidesToScroll={1}
-                    autoplay={false}
-                    autoplaySpeed={0}
-                    /> */}
                 <video 
                     src="/video/videoTelf00.mov" 
                     itemType='video/mov' 
@@ -35,11 +21,11 @@ export const SeccionAudios = () => {
             </div>
             <div className="flex flex-col items-center justify-center text-cente mt-10 z-10">
                 <span className="title title-font tracking-tighter"> guarda </span>
-                <p className="title-small rotate-2 r max-w-75 md:max-w-xl lg:max-w-5xl -mt-6 lg:-mt-14 xl:-mt-20 2xl:-mt-14">
-                    la voz
+                <p className="text-4xl md:text-6xl mb-5 md:mb-10 xl:mb-15 2xl:mb-10 tracking-tighter leading-tight rotate-2 r max-w-75 md:max-w-xl lg:max-w-5xl -mt-6 lg:-mt-14 xl:-mt-20 2xl:-mt-14">
+                    <span className="title-accent font-extrabold text-pink-500/90">la voz</span>
                 </p>
                 <p className="title title-font -mt-8 lg:-mt-16 xl:-mt-22 2xl:-mt-16">
-                    de tu <span className="title-accent font-extrabold text-pink-500/90">ABUELA</span>
+                    de tu ABUELA
                 </p>
                 <small className="title-font font-bold text-lg md:text-xl lg:text-2xl text-pink-700 mb-6 -mt-6 lg:-mt-14 xl:-mt-20 2xl:-mt-14 tracking-tighter">para siempre.</small>
                 <p className="mb-5 italic text-xl md:text-2xl text-pink-500 font-semibold underline-offset-4 underline decoration-dashed">
