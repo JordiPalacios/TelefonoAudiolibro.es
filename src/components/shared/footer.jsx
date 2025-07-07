179ea411c13280f89640579a357f2e7e7cf08a51
JordiPalacios <jordipalacioseso@gmail.com>
2025-07-07 21:14:25 +0200

    logo sin fondo añadido, footer empezado añadiendo las paginas legales

--- src/components/shared/footer.jsx
+++ src/components/shared/footer.jsx
@@ -1,30 +1,38 @@
 
 export const Footer = () => {
     return (
-        <footer className="flex flex-col items-center justify-center gap-4 py-10 bg-footer">
-            <h1 className="font-bold text-sm md:text-xl text-center">
-                telefonoaudiolibro.es
-            </h1>
-            <small className="text-xs md:text-base text-center">
-                © 2025 telefonoaudiolibro.es - Todos los derechos reservados. 
-            </small>
-            <div className="flex flex-row gap-2 items-center justify-center">
-                <a 
-                    href="https://www.instagram.com/telefonoaudiolibro/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="transition-transform duration-500 hover:scale-110"
-                >
-                    <img src="/icons/ig.svg" alt="instagram icon" className="w-9 h-9 aspect-[3/4]" loading="lazy"/>
-                </a>
-                <a 
-                    href="https://www.instagram.com/telefonoaudiolibro/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="transition-transform duration-500 hover:scale-110"
-                >
-                    <img src="/icons/tiktok.svg" alt="tiktok icon" className="w-8 h-8 aspect-[3/4]" loading="lazy"/>
-                </a>
+        <footer className="flex flex-col lg:flex-row items-center justify-center lg:justify-between lg:gap-4 py-10 bg-footer px-15">
+            <img src="/img/logo.webp" alt="logo" className="hidden lg:block w-45 h-45"/>
+            <div className="flex flex-col items-center justify-center gap-4 py-5 lg:py-10">                
+                <h1 className="font-bold text-sm md:text-xl text-center">
+                    telefonoaudiolibro.es
+                </h1>
+                <small className="text-xs md:text-base text-center">
+                    © 2025 telefonoaudiolibro.es - Todos los derechos reservados. 
+                </small>
+                <div className="flex flex-row gap-2 items-center justify-center">
+                    <a 
+                        href="https://www.instagram.com/telefonoaudiolibro/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="transition-transform duration-500 hover:scale-110"
+                        >
+                        <img src="/icons/ig.svg" alt="instagram icon" className="w-9 h-9 aspect-[3/4]" loading="lazy"/>
+                    </a>
+                    <a 
+                        href="https://www.instagram.com/telefonoaudiolibro/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="transition-transform duration-500 hover:scale-110"
+                        >
+                        <img src="/icons/tiktok.svg" alt="tiktok icon" className="w-8 h-8 aspect-[3/4]" loading="lazy"/>
+                    </a>
+                </div>
+            </div>
+            <div className="flex flex-col items-center justify-center gap-4 py-10">
+                <a href="/">Aviso Legal</a>
+                <a href="/">Política de Privacidad</a>
+                <a href="/">Términos y condiciones</a>
             </div>
         </footer>
     )
