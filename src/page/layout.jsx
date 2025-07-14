ed8e6fd43a1fb955146001b7a6dd74104386a725
JordiPalacios <jordipalacioseso@gmail.com>
2025-07-14 18:51:46 +0200

    ocultado el nav y footer una vez se ha pagado

--- src/page/layout.jsx
+++ src/page/layout.jsx
@@ -1,13 +1,15 @@
-import { Outlet } from 'react-router'
+import { Outlet, useLocation } from 'react-router'
 import { Footer, NavMenu } from '../components'
 
 export const Layout = () => {
+    const location =useLocation()
+    const hiddeNavAndFooter = location.pathname === '/reserva-realizada'
     return (
         <>
-            <NavMenu />
+            {!hiddeNavAndFooter && <NavMenu />}
             {/* Aquí renderizo los componentes de forma dinámica */}
             <Outlet />
-            <Footer />
+            {!hiddeNavAndFooter && <Footer />}
         </>
     )
 }
