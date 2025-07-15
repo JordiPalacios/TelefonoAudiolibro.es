e1bd047e514a8b4ff6dd88dbee15db35a21eacd9
JordiPalacios <jordipalacioseso@gmail.com>
2025-07-15 17:26:24 +0200

    mejorado para que si no tenemos el elemento visible nos lleve a la pagina que esta

--- src/components/shared/navSections.jsx
+++ src/components/shared/navSections.jsx
@@ -1,6 +1,8 @@
 export const NavSections = ( {section} ) => {
     const name = section.charAt(0).toUpperCase() + section.slice(1);
     
+    console.log("name", name)
+
     return (
         <a href={section}
         onClick={e => {
@@ -10,6 +12,8 @@ export const NavSections = ( {section} ) => {
                     el.scrollIntoView({ behavior: "smooth" });
                     // Quita el hash de la URL
                     window.history.replaceState(null, "", window.location.pathname);
+                    } else {
+                    window.location.href = section; 
                     }
                 }}
         className='mb-4 text-sm lg:text-lg text-white hover:text-white/70 transition-colors duration-300'>
