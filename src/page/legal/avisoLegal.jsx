2e29ba00a44b960e933f95a47d51c0b769681730
JordiPalacios <jordipalacioseso@gmail.com>
2025-07-15 18:48:50 +0200

    paginas legales done

--- src/page/legal/avisoLegal.jsx
+++ src/page/legal/avisoLegal.jsx
@@ -1,7 +1,69 @@
-import React from 'react'
+import { Section } from "../../components"
 
 export const AvisoLegal = () => {
+
     return (
-        <div>Aviso Legal</div>
-    )
+    <>
+        <Section as="main" id='avisoLegal' className="mt-20 px-10"> 
+                <h1 className="title-font text-3xl xl:text-5xl mb-5 tracking-tighter leading-tight font-extrabold underline decoration-3 underline-offset-2">Aviso Legal</h1>
+                <p className="text-start mb-5 text-lg 2xl:text-xl tracking-tighter leading-tight">
+                    <span className="subtitle-legal">
+                        1.- Información general y de contacto. 
+                    </span>
+                    <br />
+                    <br />
+                    En cumplimiento de lo dispuesto en la Ley 34/2002, de 11 de julio, de Servicios de la Sociedad de la Información y del Comercio Electrónico, los responsables de la página web Telefonoaudiolibro.es (https://telefonoaudiolibro.es), en adelante Telefonoaudiolibro.es, ponen en conocimiento de los usuarios de la presente página web los siguientes extremos, en relación con el prestador de servicios de la sociedad de la información:
+                    <br />
+                    <br />
+                    Ivet Casas Prados, provista con DNI 39474056F, con domicilio en C/ Numancia 31, 08240, Manresa, Barcelona.
+                    <br />
+                    <br />
+                    Adicionalmente, se informa a los usuarios que para el planteamiento o resolución de cualquier conflicto, incidencia o cuestión que pudiera surgir respecto del acceso a la presente página web, de su uso y / o de los contenidos puestos a disposición del público a través de la misma, quedan habilitadas las siguientes direcciones de contacto y comunicación directa y efectiva:
+                    <br />
+                    <br />
+                    - Dirección postal de contacto: <br />
+                    Ivet Casas Prados, provista con DNI 39474056F, con domicilio en C/ Numancia 31, 08240, Manresa, Barcelona.
+                    <br />
+                    <br />
+                    - Dirección electrónica de contacto: <span style={{ color : 'var(--accent)'}}>contacto@telefonoaudiolibro.es</span>
+                    <br />
+                    <br />
+                    <span className="subtitle-legal">                        
+                        2.- Responsabilidad sobre los contenidos puestos a disposición del público a través de Telefonoaudiolibro.es.
+                    </span>
+                    <br />
+                    <br />
+                    Los responsables de Telefonoaudiolibro.es ponen en conocimiento de los usuarios que realizarán las actuaciones que resulten pertinentes a fin de garantizar la exactitud y precisión de la información puesta a disposición del público a través de la página web https://telefonoaudiolibro.es procediendo a la actualización periódica de la misma y a la corrección de eventuales errores con la mayor celeridad posible.
+                    <br />
+                    <br />
+                    No obstante, los responsables de Telefonoaudiolibro.es no pueden garantizar la inexistencia de errores o la continua actualización de la información puesta a disposición del público a través de la mencionada página web.
+                    <br />
+                    <br />
+                    Igualmente, los responsables de Telefonoaudiolibro.es no puede garantizar que el acceso  a la página web https://telefonoaudiolibro.es no se vea temporalmente interrumpido por razones de índole técnica, ni que los contenidos a los que accedan los usuarios, o el software que sirve de base a los mismos, se encuentren permanentemente libres de errores.
+                    <br />
+                    <br />
+                    En consecuencia,  los responsables de Telefonoaudiolibro.es no asumirán responsabilidad alguna, ya sea directa o indirecta, respecto de cualquier perjuicio causado a los usuarios que venga provocado o pudiera derivarse de interferencias, omisiones, interrupciones, averías telefónicas o desconexiones en el funcionamiento operativo del sistema electrónico, motivados por causas ajenas a Telefonoaudiolibro.es , así como respecto de cualquier daño o perjuicio causado por la actuación de terceras personas mediante intromisiones ilegítimas en el sistema operativo, ya sea en forma de malware, virus, gusanos, troyanos, código malicioso, o cualesquiera otras figuras análogas.
+                    <br />
+                    <br />
+                    Asimismo,  los responsables de Telefonoaudiolibro.es quedan excluidos de cualquier responsabilidad que pudiera derivarse de retrasos  o bloqueos en el funcionamiento  operativo del sistema electrónico, que vengan causados por deficiencias o sobrecarga en las líneas telefónicas o de Internet.
+                    <br />
+                    <br />
+                    En todo caso, el acceso a la página web https://telefonoaudiolibro.es,  así como el uso que se efectúe de la información puesta a disposición del público a través de la misma, son realizados bajo la exclusiva y única responsabilidad del usuario.
+                    <br />
+                    <br />
+                    Los responsables de Telefonoaudiolibro.es no asumirán responsabilidad alguna, ya sea directa o indirecta, respecto de ninguna consecuencia, daño o perjuicio, de cualquier índole, que eventualmente pudiera sufrir el usuario y que venga originado como consecuencia del acceso a la información puesta a disposición del público a través de la página web Telefonoaudiolibro.es .
+                    <br />
+                    <br />
+                    Los responsables de Telefonoaudiolibro.es se reservan el derecho a efectuar, en cualquier momento y sin previo aviso, modificaciones, supresiones o actualizaciones respecto de la información puesta a disposición del público a través de Telefonoaudiolibro.es , en su configuración interna o en su presentación.
+                    <br />
+                    <br />
+                    Los responsables de Telefonoaudiolibro.es, se reservan el derecho a suspender temporalmente y sin previo aviso, la accesibilidad a la página web referida, con motivo de operaciones de mantenimiento, reparación, actualización o mejora de las mismas.
+                    <br />
+                    <br />
+                    Por último, los responsables de Telefonoaudiolibro.es ponen en conocimiento de los usuarios que se encuentran puestos a disposición de los mismos, a través de la presente página web, los documentos denominados Terminos y Condiciones y Politica de Privacidad, mediante cuya lectura los usuarios ostentan la posibilidad de ampliar su información en lo relativo a las condiciones de uso y navegación a través del presente sitio web, así como en lo relativo al compromiso de los responsables de Telefonoaudiolibro.es con la privacidad y confidencialidad de los datos de carácter personal suministrados por los usuarios, respectivamente.
+                </p>
+        </Section>
+        <div className='w-full bg-white' style={{ height:"4px" }} />
+    </>
+)
 }
